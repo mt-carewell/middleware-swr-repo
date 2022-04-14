808df20e903f3b2930253c7c45a8b996d9b83c45
Michael Thomas <michael.thomas@consultant.carewell.com>
2022-04-14 07:46:03 +0100

    add a redirect

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return (
+      [
+        {
+          source: "/redirect/me/please",
+          destination: "/",
+          permanent: true
+        }
+      ]
+    )
+  }
 }
 
 module.exports = nextConfig
