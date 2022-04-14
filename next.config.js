b7fb64ddb15796493375b940d9e3b8cdd967204d
Michael Thomas <michael.thomas@consultant.carewell.com>
2022-04-14 08:16:12 +0100

    trailing slash

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  trailingSlash: true,
   async redirects() {
     return (
       [
         {
-          source: "/redirect/me/please",
+          source: "/redirect/me/please/",
           destination: "/",
           permanent: true
         }
